c615f698cf21f4bc1017ab466e1b2d43945fcc06
Shanglin <691573307@qq.com>
2022-05-13 04:52:17 +1000

    Add a interface for tickets

---
+++ Controller/ticketsController.swift
@@ -0,0 +1,23 @@
+//
+//  ticketsController.swift
+//  Ticketo
+//
+//  Created by Shanglin Jiang on 12/5/2022.
+//
+
+import Foundation
+import UIKit
+
+class ticketsController: UIViewController {
+
+    @IBOutlet weak var haha: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
