ad803cc1495594c05a778d49a250c3e69659215d
SeanQQQ <sean.savanah1999@gmail.com>
2022-05-15 14:53:59 +1000

    added data controller AddUser, LoginUser methods

--- Controller/DataController.swift
+++ Controller/DataController.swift
@@ -11,7 +11,7 @@ import CoreData
 import UIKit
 import CoreData
 
-struct Flite{ //An Bug with XCode has prevented me from naming this struct Flight, the name is reserved by a generated file which i cannot delete
+struct flightStruct{
     var to: String
     var from: String
     var date: String
@@ -19,31 +19,33 @@ struct Flite{ //An Bug with XCode has prevented me from naming this struct Fligh
     
 }
 
+struct userStruct{
+    var username: String
+    var password: String
+}
+
 class DataController{
     
-    static func getFlights() -> [Flite] {
+    static func getFlights() -> [flightStruct] { //In Production this will access an online Db, but for demonstration purposes it returns a hardcoded array of Flights
         let flightList = [
-            Flite(to: "Calais", from: "London", date: "5/5/2022", id: 1),
-            Flite(to: "Moscow", from: "Kyoto", date: "2/6/2022", id: 2),
-            Flite(to: "Tokyo", from: "Sydney", date: "7/8/2022", id: 3),
-            Flite(to: "Perth", from: "Abu Dhabi", date: "1/6/2022", id: 4)
+            flightStruct(to: "Calais", from: "London", date: "5/5/2022", id: 1),
+            flightStruct(to: "Moscow", from: "Kyoto", date: "2/6/2022", id: 2),
+            flightStruct(to: "Tokyo", from: "Sydney", date: "7/8/2022", id: 3),
+            flightStruct(to: "Perth", from: "Abu Dhabi", date: "1/6/2022", id: 4)
         ]
         
         return flightList
     }
     
-    func addUser(username: String, password: String) {
+    static func addUser(username: String, password: String) {
       
         guard let appDelegate =
                 UIApplication.shared.delegate as? AppDelegate else {
                     return
                 }
       
-        let context =
-            appDelegate.persistentContainer.viewContext
-      
-        let entity =
-            NSEntityDescription.entity(forEntityName: "Person", in: context)!
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: context)!
       
         let user = NSManagedObject(entity: entity, insertInto: context)
 
@@ -52,9 +54,45 @@ class DataController{
 
         do {
             try context.save()
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
+        } catch {
+            print("Failure")
         }
+    }
+    
+    static func getUserAndAuth(username: String, password: String) -> userStruct{
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                    return userStruct(username: "ERR0", password: "ERR0")
+                }
+      
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSManagedObject>(entityName: "User")
+        
+        var Users: [NSManagedObject] = []
+        
+        do{
+            Users = try context.fetch(request)
+        }catch{
+            print("Fetch Failed")
+        }
+        
+        for user in Users {
+            let currentUsername = user.value(forKey: "username") as! String
+            let currentPassword = user.value(forKey: "password") as! String
+            
+            if(currentUsername == username){
+                if(currentPassword == password){
+                    return userStruct(username: currentUsername, password: currentPassword)
+                }else{
+                    print("not authorized")
+                    return userStruct(username: "ERR2", password: "ERR2")
+                }
+            }
+        }
+        
+        print("user not found")
+        return userStruct(username: "ERR1", password: "ERR1")
     }
 }
 
