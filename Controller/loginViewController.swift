420d447c853f26d6923ef927e5b01f624d481a98
vincent0716-hub <vincentlin968@gmail.com>
2022-05-17 19:34:19 +1000

    add loginpagecontroller add segue

---
+++ Controller/loginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  loginViewController.swift
+//  Ticketo
+//
+//  Created by Vincent on 2022/5/17.
+//
+
+import Foundation
+import UIKit
+
+class loginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+}
