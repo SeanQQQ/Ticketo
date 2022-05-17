c7315c216cd0725402015bd79281b3deaf39560d
vincent0716-hub <vincentlin968@gmail.com>
2022-05-18 02:24:55 +1000

    complete the "popup" register UIpage and already test it aswell

---
+++ Controller/PopUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PopUpViewController.swift
+//  Ticketo
+//
+//  Created by Vincent on 2022/5/18.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePopUp(_ sender: AnyObject) {
+        self.view.removeFromSuperview()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Controller/loginViewController.swift
+++ Controller/loginViewController.swift
@@ -16,4 +16,20 @@ class loginPage: UIViewController {
         
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showSignInPopup(_ sender: AnyObject) {
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! PopUpViewController
+        self.addChild(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove(toParent: self)
+    }
+    
+    @IBAction func showPopup(_ sender: Any) {
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! PopUpViewController
+        self.addChild(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove(toParent: self)
+    }
 }
