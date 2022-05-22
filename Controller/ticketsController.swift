a8a77e9cf6f9fa1ad8233178ea6b39374b793592
Sean <sean.savanah1999@gmail.com>
2022-05-22 23:46:53 +1000

    :((((((

--- Controller/ticketsController.swift
+++ Controller/ticketsController.swift
@@ -14,6 +14,18 @@ class ticketsController: UIViewController {
     
     @IBOutlet weak var txt_origin1: UITextField!
     
+    @IBOutlet weak var from1: UITextField!
+    
+    @IBOutlet weak var to1: UITextField!
+    @IBOutlet weak var date1: UITextField!
+    
+    @IBOutlet weak var from2: UITextField!
+    
+    @IBOutlet weak var to2: UITextField!
+    
+    @IBOutlet weak var date2: UITextField!
+    
+    
     var username = "Test"
     
     override func viewDidLoad() {
@@ -26,7 +38,22 @@ class ticketsController: UIViewController {
     private func loadTicketText(){
         let x = DataController.GetUserTickets(username: username);
         print(x)
-        //print(x[0].from)
+      
+        if(x.count == 1){ //I know this is bad but we were running out of time
+            from1.text = x[0].from;
+            to1.text = x[0].to;
+        }
+        if(x.count > 2){
+            from1.text = x[0].from;
+            from2.text = x[1].from;
+            to1.text = x[0].to;
+            to2.text = x[1].to;
+                
+        }
+            
+     
+
+
         //print(x[0].to)
         //print(x[0].date)
 
